b21bacd8a68716cdf286131295fc8a6251b2227d
mordjane <78384914+mordjanee@users.noreply.github.com>
2024-04-11 10:18:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ def get_weather(latitude, longitude):
     return data
 
 if __name__ == "__main__":
-    latitude = float(input("Entrez la latitude du lieu : "))
-    longitude = float(input("Entrez la longitude du lieu : "))
+    latitude = float(os.environ.get("LAT", 0.0))
+    longitude = float(os.environ.get("LONG", 0.0))
     weather_data = get_weather(latitude, longitude)
     print(weather_data)
 
-#https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=aaca89a14b3698bbd46dd966b2fa436d
+#https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=236df28640d8e0bc71a29bd329f6a8d7
+
